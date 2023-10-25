e3256872400fdc92925c7d83d70666d5c29def70
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2023-10-25 18:13:59 +0300

    hw6-js

--- js/task-10.js
+++ js/task-10.js
@@ -1,25 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
+    .toString(16)                                                         
     .padStart(6, 0)}`;
 }
 
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]')
-const input = document.querySelector('input[type="number"]');
+const createBoxes = document.querySelector('[data-create]');
+const destroyBoxes = document.querySelector('[data-destroy]')
+const amount = document.querySelector('input[type="number"]');
 
-createButton.addEventListener('click', function() { 
-  const value = parseInt(input.value);
+createBoxes.addEventListener('click', function() { 
+  const value = parseInt(amount.value);
 
   for (let i = 0; i < value; i++) {
     const boxes = document.createElement('div');
-    boxes.style.width = '30px';
+    boxes.style.width = (30 + (i * 10)) + 'px';
     boxes.style.height = '30px';
+
     boxes.style.backgroundColor = getRandomHexColor();
     document.body.appendChild(boxes);
 
-    destroyButton.addEventListener('click', function() {
+    destroyBoxes.addEventListener('click', function() {
       boxes.remove();
     });
   }
 });
+
+
+
